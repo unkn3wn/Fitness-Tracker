7c97d8745d97c557939f88c9d186534a0a94db96
unkn32n <reyesfernando151@gmail.com>
2022-11-08 09:22:50 -0800

    working on server part

--- server.js
+++ server.js
@@ -1,31 +1,47 @@
-require("dotenv").config();
+const http = require("http");
 
-const express = require("express");
-const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
-const {COOKIE_SECRET} = process.env;
-const {authRequired} = require("./routes/utils");
-const client = require("./db/client");
+const {PORT = 8080} = process.env;
 
-const app = express();
-const PORT = 8080;
+const server = http.createServer(app);
 
-client.connect();
-
-const router = require("./routes")
-//Middleware
-
-app.use(morgan("dev"));
-app.use(cookieParser(COOKIE_SECRET));
-app.use(express.json());
-
-//routers
-app.use("/api", router);
-app.get("/test", authRequired, (req, res, next)=>{
-    res.send("You are auth")
+server.listen(PORT,()=>{
+    console.log(`port ${PORT}`)
 })
 
-//EROR HANDLER
-app.use((err,req,res,next)=>{
-    res.status(500).send(err);
-})
+// require("dotenv").config();
+
+// const express = require("express");
+// const morgan = require("morgan");
+// const cookieParser = require("cookie-parser");
+// const {COOKIE_SECRET} = process.env;
+// const {authRequired} = require("./routes/utils");
+// const client = require("./db/client");
+
+// const app = express();
+// const PORT = 8080;
+
+// client.connect();
+
+// const router = require("./routes");
+// const { server } = require("http");
+// //Middleware
+
+// app.use(morgan("dev"));
+// app.use(cookieParser(COOKIE_SECRET));
+// app.use(express.json());
+
+// //routers
+// app.use("/api", router);
+// app.get("/test", authRequired, (req, res, next)=>{
+//     res.send("You are auth")
+// })
+// console.log("server running")
+// //EROR HANDLER
+// app.use((err,req,res,next)=>{
+//     res.status(500).send(err);
+// })
+
+
+// server.listen(PORT,()=>{
+//     console.log(`APP LISTENING ON PORT ${PORT}`)
+// })
