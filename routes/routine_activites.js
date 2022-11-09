8e8a2fefb6a777150e61d463774570a627b4c13b
David Masoud <davidmasoud24@gmail.com>
2022-11-09 10:08:22 -0800

    Routes Done

--- routes/routine_activites.js
+++ routes/routine_activites.js
@@ -25,3 +25,70 @@ routineActivitiesRouter.post("/", async (req, res, next) => {
     next({ message });
   }
 });
+
+routineActivitiesRouter.patch(
+  "/:routineActivityId",
+  authRequired,
+  async (req, res, next) => {
+    const { routineActivityId } = req.params;
+    const { count, duration } = req.body;
+    const updateFields = {};
+
+    if (count) {
+      updateFields.count = count;
+    }
+    if (duration) {
+      updateFields.duration = count;
+    }
+    try {
+      const oRa = await getRoutineActivtyByid(routineActivityId);
+      const oR = await getRoutineById(oRa.routine_id);
+      if (oR.creator_id === req.user.id) {
+        const uRa = await updateRoutineActivity(
+          routineActivityId,
+          updateFields
+        );
+        res.send({ routine: uRa });
+      } else {
+        next({
+          name: "User error unauthorized",
+          message: "You cannot update routine_activity through this user",
+        });
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  }
+);
+
+routineActivitiesRouter.delete(
+  "/:routineActivityId",
+  authRequired,
+  async (req, res, next) => {
+    try {
+      const { routineActivityId } = req.params;
+      const routine = await getRoutineActivtyByid(routine);
+      const oR = await getRoutineById(routineActivityId.routine_id);
+      if (oR.creator_id === req.user.id) {
+        destroyRoutineActivity(routineActivityId);
+        res.send({ Deleted: routine });
+      } else {
+        next(
+          routine
+            ? {
+                name: "User error Unauthorized",
+                message: "You cannot delete routine_activity through this user",
+              }
+            : {
+                name: "Error, routine_activity not found",
+                message: "Routine_activity does not exist",
+              }
+        );
+      }
+    } catch ({ message, name }) {
+      next({ name, message });
+    }
+  }
+);
+
+module.exports = routineActivitiesRouter;
